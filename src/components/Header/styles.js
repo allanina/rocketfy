f2f768510e3eee8480bda4e590439e154b4063cf
Allana Evellyn <allanaevellynm@gmail.com>
2022-04-01 22:19:42 -0300

    first commit

---
+++ src/components/Header/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    height: 60px;
+    padding: 0 30px;
+    background: #5EBFBF;
+    color: #fff;
+    font-size: 10px;
+
+    display: flex;
+    align-items: center;
+`;
